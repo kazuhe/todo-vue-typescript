6638a1ebdb55352b42ba4ed5c90bd1ec82ba9a34
kazuhe <omc.ohara@gmail.com>
2023-02-10 16:30:35 +0900

    Tailwind CSS をインストール

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <div>
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
