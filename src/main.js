9f53a96600b5be10132a5f942a23f975d08234c6
kazuhe <omc.ohara@gmail.com>
2023-02-10 23:05:24 +0900

    MSW をインストール

--- src/main.js
+++ src/main.js
@@ -2,4 +2,12 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
+/** msw を利用して API をモックする */
+if (import.meta.env.DEV) {
+  (async () => {
+    const { worker } = await import("./mocks/browser");
+    worker.start();
+  })();
+}
+
 createApp(App).mount('#app')
